fd4dd50625e9ec1005f9912e207ddb4a94acad8a
Carson Yong <CarsonYong@gmail.com>
2015-07-27 19:48:40 +0000

    first commit

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,46 @@
+class MoviesController < ApplicationController
+
+	def index
+		@movies = Movie.all
+	end
+
+	def show
+		@movie = Movie.find(params[:id])
+	end
+
+	def new
+		@movies = Movie.new
+	end
+
+	def edit
+		@movie = Movie.find(params[:id])
+	end
+
+	def create
+		@movie = Movie.new(movie_params)
+
+		if @movie.save
+			redirect_to movies_path
+		else
+			render :new
+		end
+	end
+	def update
+		@movie = Movie.find(params[:id])
+
+		if @movie.update_attributes(movie_params)
+			redirect_to movies_path(@movie)
+		else
+			render :edit
+		end
+	end
+
+	protected
+
+	def movie_params
+		params.requrie(:movie).permit(
+			:title, :relase_date, :director, :runtime_in_minutes, :poster_image_url,
+		)
+	end
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Movie.create(title: '500 Days of Summer', director: 'Marc Webb', runtime_in_minutes: 95, description: 'Tom (Joseph Gordon-Levitt), greeting-card writer and hopeless romantic, is caught completely off-guard when his girlfriend, Summer (Zooey Deschanel), suddenly dumps him. He reflects on their 500 days together to try to figure out where their love affair went sour, and in doing so, Tom rediscovers his true passions in life.', poster_image_url: 'http://www.gstatic.com/tv/thumb/dvdboxart/193428/p193428_d_v7_aa.jpg')
